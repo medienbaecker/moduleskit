f015634fc03cd8a0df6241d2b2a3e3c944bab33e
Thomas Günther <mail@medienbaecker.com>
2026-05-26 11:12:51 +0200

    Install modules plugin

---
+++ site/plugins/modules/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'medienbaecker/kirby-modules',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd59c9a55e6b8446842e8810b34c1da341665f222',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'medienbaecker/kirby-modules' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd59c9a55e6b8446842e8810b34c1da341665f222',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
