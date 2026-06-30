fad2b84b52444c577eae2c7583d4cdec593abd5f
Thomas Günther <mail@medienbaecker.com>
2026-06-30 11:18:02 +0200

    Rework example modules and seed clean content

--- site/snippets/modules/images.php
+++ site/snippets/modules/images.php
@@ -1,5 +1,5 @@
 <div id="<?= $module->slug() ?>" class="<?= $module->moduleId() ?>">
-  <?php foreach ($module->files()->sorted() as $image): ?>
+  <?php foreach ($module->gallery()->toFiles() as $image): ?>
     <?= $image->crop(400, 300) ?>
   <?php endforeach ?>
 </div>
